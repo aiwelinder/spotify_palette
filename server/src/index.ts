4b57e98bd1e4db8aef4ee87657a8fffd343895a9
Nali Welinder <aiwelinder@gmail.com>
2026-06-05 14:28:51 -0700

    fix port processing :P

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = Number(process.env.PORT) || 3001;
 
 app.use(cors());
 app.use(express.json());
